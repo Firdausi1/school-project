86dec25a59ee92f560272f4f2605703aa9092226
Firdausy Bashir <43407297+Firdausi1@users.noreply.github.com>
2020-06-07 19:44:44 +0100

    added app.js file

---
+++ app.js
@@ -0,0 +1,8 @@
+let menuButton = document.querySelector(".nav-btn");
+let menu = document.querySelector(".nav-links");
+function showNav(){
+    menuButton.style.display = 'none';
+    menu.style.display = 'block'
+    menu.style.marginLeft = 0
+
+}
