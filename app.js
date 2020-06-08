ef901963f0205117d899ebdc32b84db13e3933ba
Firdausy Bashir <43407297+Firdausi1@users.noreply.github.com>
2020-06-08 17:22:27 +0100

    Slide show added

--- app.js
+++ app.js
@@ -21,4 +21,34 @@ const navSlide = () => {
    
 }
 
+let slideIndex = 0
+const slide = () => {
+    let i;
+    const slides = document.querySelectorAll('.testimonials-text')
+    const dots = document.querySelectorAll('.scroll span')
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none'
+    }
+    for(i = 0; i < dots.length; i++){
+        dots[i].className = dots[1].className.replace(" active", "")
+    }
+    slideIndex++
+    if(slideIndex>slides.length){
+        slideIndex = 1
+    }
+    slides[slideIndex-1].style.display = 'block'
+    dots[slideIndex-1].className += " active"
+
+
+    setTimeout(slide, 5000)
+}
+
+slide();
 navSlide();
+
+// if(slides[i].style.display === 'block'){
+//     slides[i].style.display = 'none'
+// }
+// else{
+//     slides[i].style.display = 'block'
+// }
